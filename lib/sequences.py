21093c1370942c0cca7b9b78bec8b8f3bfeb847e
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-12 22:00:34 -0400

    practiced sequence loop function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    fib_range = []
+    if length > 0:
+        fib_range.append(0)
+    if length >=2:
+        fib_range.append(1)
+        for i in range(2, length):
+            fib_range.append(fib_range[-1] + fib_range[-2])
+
+    print(fib_range)
